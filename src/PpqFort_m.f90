8df4c457ec0498e64cfd33a6e427e356e08de01e
Damian Rouson <rouson@lbl.gov>
2025-11-11 16:07:31 -0800

    chore(PpqN): actually indent associates & block
    A previous commit appended _m to a file name, but neglectd to to
    make the other changes (above) stated in ther commit message.
    This commit actually makes the changes.

--- src/PpqFort_m.f90
+++ src/PpqFort_m.f90
@@ -278,33 +278,27 @@ module PpqFort
       integer :: i
   
       associate(maxx => max(1.1 * max(Ep, Eq), max(Ep, Eq) + 10))
-
-      associate(resolution => merge(0.002_c_double, 0.01d0_c_double, maxx < 15))
-
-      integrate_PpqFullN: &
-      associate(npts => int((maxx - minx) / resolution) + 1)
-
-      ! build the array of energies
-      associate(er_arr => [(minx + (i - 1) * resolution, i = 1, npts)])
-
-      block
-      real(c_double) integral
+        associate(resolution => merge(0.002_c_double, 0.01d0_c_double, maxx < 15))
+          integrate_PpqFullN: &
+          associate(npts => int((maxx - minx) / resolution) + 1)
+            define_recoil_energies: &
+            associate(er_arr => [(minx + (i - 1) * resolution, i = 1, npts)])
+               block
+               real(c_double) integral
 #if ! PREFER_DO_CONCURRENT
-      integral = sum([(PpqFullN(er_arr(i), Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10), i = 1, npts)])
+                 integral = sum([(PpqFullN(er_arr(i), Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10), i = 1, npts)])
 #else
-      ! Optimizable loop
-      integral = 0.
-      do concurrent(i = 1:npts) default (none) reduce(+: integral) shared(er_arr, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-          integral = integral + PpqFullN(er_arr(i), Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-      end do
-#endif
-      res = integral * resolution
-      end block
+                 integral = 0.
+                 do concurrent(i = 1:npts) default (none) reduce(+: integral) shared(er_arr, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
+                     integral = integral + PpqFullN(er_arr(i), Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
+                 end do
+#endif    
+                 res = integral * resolution
+               end block
+            end associate define_recoil_energies
+          end associate integrate_PpqFullN
+        end associate
       end associate
-      end associate integrate_PpqFullN
-      end associate
-      end associate
-  
   end function PpqN
 
   pure function PpqG(Ep, Eq, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqG")
