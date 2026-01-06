9183166290a4bffbe36eabf85c7dcde833c347c7
Amy Roberts <amy.roberts@ucdenver.edu>
2026-01-06 16:47:24 -0700

    refac(PpqFort_*):make all remaining functions submodules

--- src/PpqFort_m.f90
+++ src/PpqFort_m.f90
@@ -32,275 +32,100 @@ module PpqFort_m
       real(c_double), value :: Ep, Eq, F0, s, eps, V, p0, p10, q0, q10
       real(c_double) :: res
     end function PpqG
-    
+
+   pure module function Y(Er, a, b) result(res) bind(c, name="Y")
+       !! the ionization yield, using the "standard" CDMS model \(a E_r^b\)
+       real(c_double), value :: Er, a, b
+       real(c_double) :: res
+    end function Y
+ 
+    pure module function F(Er, F0, s) result(res) bind(c, name="F")
+      !! Model the Fano factor as a linear function
+      real(c_double), value :: Er, F0, s
+      real(c_double) :: res
+   end function F
+
+   pure module function Nbar(Er, a, b, eps) result(res) bind(c, name="Nbar")
+     !! average number of electron-hole pairs for a given Er
+     real(c_double), value :: Er, a, b, eps
+     real(c_double) :: res
+   end function Nbar
+
+   pure module function sigp(Ep, eps, V, p0, p10) result(res) bind(c, name="sigp")
+      !! phonon sensor resolution
+      real(c_double), value :: Ep, eps, V, p0, p10
+      real(c_double) :: res
+   end function sigp
+
+   pure module function sigq(Eq, q0, q10) result(res) bind(c, name="sigq")
+   !! charge sensor resolution
+      real(c_double), value :: Eq, q0, q10
+      real(c_double) :: res
+   end function sigq
+
+  pure module function PErN(Er) result(res) bind(c, name="PErN")
+    !! the Er (energy) distribution of neutrons
+    real(c_double), value :: Er
+    real(c_double) :: res
+  end function PErN
+
+  pure module function PErG(Er) result(res) bind(c, name="PErG")
+    !! the Er (energy) distribution of gamma events/electron recoils
+    real(c_double), value :: Er
+    real(c_double) :: res
+  end function PErG
+
+  pure module function aN(Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="aN")
+    real(c_double), value :: Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10
+    real(c_double) :: res
+  end function aN
+
+  pure module function bN(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="bN")
+    ! Input arguments
+    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
+    real(c_double) :: res
+  end function bN
+
+  pure module function cN1(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="cN1")
+    ! Input arguments
+    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
+    real(c_double) :: res
+  end function cN1
+
+  pure module function cN2(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="cN2")
+    ! Input arguments
+    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
+    real(c_double) :: res
+  end function cN2
+
+  pure module function PpqExp(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqExp")
+    ! Input arguments
+    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
+    real(c_double) :: res
+  end function PpqExp
+
+  pure module function PpqPreExp(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqPreExp")
+    ! Input arguments
+    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
+    real(c_double) :: res
+  end function PpqPreExp
+
+  pure module function PpqFullN(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqFullN")
+    ! Input arguments
+    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
+    real(c_double) :: res
+  end function PpqFullN
+
+  pure module function PpqFullG(Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqFullG")
+    ! Input arguments
+    real(c_double), value :: Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10
+    real(c_double) :: res
+  end function PpqFullG
+
   end interface
 
   ! Define pi
   real(c_double), parameter :: pi = 3.14159265358979323846_c_double
     ! TODO: move the pi definition to just above the submodule's "contains" statement
-
-contains ! TODO: remove this contains statement after all procedure definitions have been moved to the submodule
-
-   pure function Y(Er, a, b) result(res) bind(c, name="Y")
-       !! the ionization yield, using the "standard" CDMS model \(a E_r^b\)
-       real(c_double), value :: Er, a, b
-       real(c_double) :: res
-
-       ! Compute the ionization yield
-       res = a * abs(Er)**b
-    end function Y
- 
-    pure function F(Er, F0, s) result(res) bind(c, name="F")
-      !! Model the Fano factor as a linear function
-      real(c_double), value :: Er, F0, s
-      real(c_double) :: res
-
-      res = F0 + s * Er
-   end function F
-
-   pure function Nbar(Er, a, b, eps) result(res) bind(c, name="Nbar")
-     !! average number of electron-hole pairs for a given Er
-     real(c_double), value :: Er, a, b, eps
-     real(c_double) :: res
-
-     res = Y(Er, a, b) * Er / eps
-   end function Nbar
-
-   pure function sigp(Ep, eps, V, p0, p10) result(res) bind(c, name="sigp")
-      !! phonon sensor resolution
-      real(c_double), value :: Ep, eps, V, p0, p10
-      real(c_double) :: res
-      real(c_double) :: e2pre, e2e
-
-      e2pre = (p10**2 - p0**2)
-      e2e = (Ep / (10.0d0 * (1.0d0 + (V / eps / 1000.0d0))))**2
-      res = sqrt(p0**2 + e2pre * e2e)
-   end function sigp
-
-   pure function sigq(Eq, q0, q10) result(res) bind(c, name="sigq")
-   !! charge sensor resolution
-      real(c_double), value :: Eq, q0, q10
-      real(c_double) :: res
-      real(c_double) :: e2pre, e2e
-
-      e2pre = (q10**2 - q0**2)
-      e2e = (Eq / 10.0d0)**2
-      res = sqrt(q0**2 + e2pre * e2e)
-   end function sigq
-
-  pure function PErN(Er) result(res) bind(c, name="PErN")
-    !! the Er (energy) distribution of neutrons
-    real(c_double), value :: Er
-    real(c_double) PNa, PNb, PNd
-    real(c_double) :: res
-
-    ! we don't ever change these parameters
-    ! they were determined by fits to Geant output
-    PNa = 0.53693208d0
-    PNb = 6.41515782d0
-    PNd = 23.71789286d0
-
-    if (Er < 0.0d0) then
-      res = 0.0d0
-    else
-      res = PNa * (1.0d0 / PNb) * exp(-Er / PNb) + &
-            (1.0d0 - PNa) * (1.0d0 / PNd) * exp(-Er / PNd)
-    end if
-  end function PErN
-
-  pure function PErG(Er) result(res) bind(c, name="PErG")
-    !! the Er (energy) distribution of gamma events/electron recoils
-    real(c_double), value :: Er
-    real(c_double) :: PGa, PGb, PGd
-    real(c_double) :: res
-
-    ! Set values for PGa, PGb, and PGd
-    ! we don't ever change these parameters
-    ! they were determined by fits to Geant output
-    PGa = 0.573211975d0
-    PGb = 0.169520023d0
-    PGd = 279.552394d0
-
-    if (Er < 0.0d0) then
-      res = 0.0d0
-    else
-      res = PGa * (1.0d0 / PGb) * exp(-Er / PGb) + &
-            (1.0d0 - PGa) * (1.0d0 / PGd) * exp(-Er / PGd)
-    end if
-  end function PErG
-
-  pure function aN(Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="aN")
-    real(c_double), value :: Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10
-    real(c_double) :: t1, t2, t3, res
-    
-    t1 = (V/1.0d3)*(Ep - Er) / sigp(Ep, eps, V, p0, p10)**2
-    t2 = eps*Eq / sigq(Eq, q0, q10)**2
-    t3 = 1 / F(Er, F0, s)
-    
-    res = t1 + t2 + t3
-  end function aN
-
-  pure function bN(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="bN")
-    ! Input arguments
-    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
-
-    ! Local variables
-    real(c_double) :: t1, t2, t3
-
-    ! Output
-    real(c_double) :: res
-
-    ! Compute the result
-    t1 = 1.0d0 / (2.0d0 * Nbar(Er, a, b, eps) * F(Er, F0, s))
-    t2 = eps**2 / (2.0d0 * sigq(Eq, q0, q10)**2)
-    t3 = V**2 / (2.0d0 * (sigp(Ep, eps, V, p0, p10) * 1.0d3)**2)
-
-    res = t1 + t2 + t3
-  end function bN
-
-  pure function cN1(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="cN1")
-    ! Input arguments
-    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
-
-    ! Local variables
-    real(c_double) :: t1, t2, t3
-
-    ! Output
-    real(c_double) :: res
-
-    ! Compute the result
-    t1 = -((Ep - Er)**2) / (2.0d0 * (sigp(Ep, eps, V, p0, p10)**2))
-    t2 = -(Eq**2) / (2.0d0 * (sigq(Eq, q0, q10)**2))
-    t3 = -Nbar(Er, a, b, eps) / (2.0d0 * F(Er, F0, s))
-
-    res = t1 + t2 + t3
-  end function cN1
-
-  pure function cN2(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="cN2")
-    ! Input arguments
-    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
-
-    ! Local variables
-    real(c_double) :: sigp_val, sigq_val, Nbar_val, F_val
-
-    ! Output
-    real(c_double) :: res
-
-    ! Compute intermediate values
-    sigp_val = sigp(Ep, eps, V, p0, p10)
-    sigq_val = sigq(Eq, q0, q10)
-    Nbar_val = Nbar(Er, a, b, eps)
-    F_val = F(Er, F0, s)
-
-    ! Compute the result
-    res = 1.0d0 / (2.0d0 * pi * sqrt(2.0d0 * pi) * sigp_val * sigq_val * sqrt(Nbar_val * F_val))
-  end function cN2
-
-  pure function PpqExp(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqExp")
-    ! Input arguments
-    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
-
-    ! Local variables
-    real(c_double) :: cN1_val, aN_val, bN_val
-
-    ! Output
-    real(c_double) :: res
-
-    ! Compute the components of the exponent
-    cN1_val = cN1(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-    aN_val = aN(Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10)
-    bN_val = bN(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-
-    ! Compute the result
-    res = cN1_val + (aN_val**2) / (4.0d0 * bN_val)
-  end function PpqExp
-
-  pure function PpqPreExp(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqPreExp")
-    ! Input arguments
-    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
-
-    ! Local variables
-    real(c_double) :: exponent, cN2_val, bN_val
-
-    ! Output
-    real(c_double) :: res
-
-    ! Compute the exponent
-    exponent = PpqExp(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-   
-    ! first we test that the exponent won't cause a machine underflow
-    if (exponent < -700.0d0) then
-      res = 0.0d0
-    else
-      ! Compute the result
-      ! if the exponent is not too small
-      cN2_val = cN2(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-      bN_val = bN(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-
-      res = 0.5d0 * sqrt(pi) * cN2_val * exp(exponent) * (1.0d0 / sqrt(bN_val))
-    end if
-  end function PpqPreExp
-
-  pure function PpqFullN(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqFullN")
-    ! Input arguments
-    real(c_double), value :: Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10
-
-    ! Local variables
-    real(c_double) :: prefac, erffac, aN_val, bN_val
-
-    ! Output
-    real(c_double) :: res
-
-    ! Compute the prefactor
-    prefac = PpqPreExp(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-
-    if (prefac > 0) then
-      ! Compute aN and bN values
-      ! if it's worth it
-      aN_val = aN(Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10)
-      bN_val = bN(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-
-      ! Compute the error function factor
-      erffac = erf(aN_val / (2.0d0 * sqrt(bN_val))) + 1.0d0
-
-      ! Compute the result
-      res = prefac * erffac * PErN(Er)
-    else
-      res = 0
-    end if
-  end function PpqFullN
-
-  pure function PpqFullG(Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10) result(res) bind(c, name="PpqFullG")
-    ! Input arguments
-    real(c_double), value :: Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10
-
-    ! Local variables
-    real(c_double) :: a, b, prefac, erffac, aN_val, bN_val
-
-    ! Output
-    real(c_double) :: res
-
-    ! the yield is always 1.0 for gamma (electron recoil) interactions
-    a = 1.0
-    b = 0.0
-
-    ! Compute the prefactor
-    prefac = PpqPreExp(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-
-    if (prefac > 0) then
-      ! Compute aN and bN values
-      ! if it's worth it
-      aN_val = aN(Er, Ep, Eq, F0, s, eps, V, p0, p10, q0, q10)
-      bN_val = bN(Er, Ep, Eq, a, b, F0, s, eps, V, p0, p10, q0, q10)
-
-      ! Compute the error function factor
-      erffac = erf(aN_val / (2.0d0 * sqrt(bN_val))) + 1.0d0
-
-      ! Compute the result
-      res = prefac * erffac * PErG(Er)
-    else
-      res = 0
-    end if
-  end function PpqFullG
 
 end module PpqFort_m
